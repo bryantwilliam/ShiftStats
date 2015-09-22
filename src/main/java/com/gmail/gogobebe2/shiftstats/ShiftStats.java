56d5ccc622b9f146a020ba1e834ecab7999b1648
gogobebe2 <gogobebe2@gmail.com>
2015-09-23 07:11:20 +1200

    1.0.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftstats/ShiftStats.java
+++ src/main/java/com/gmail/gogobebe2/shiftstats/ShiftStats.java
@@ -3,9 +3,16 @@ package com.gmail.gogobebe2.shiftstats;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShiftStats extends JavaPlugin {
+    public static String CONFIG_HOSTNAME = "HostName";
+    public static String CONFIG_PORT = "Port";
+    public static String CONFIG_DATABASE = "Database";
+    public static String CONFIG_USERNAME = "Username";
+    public static String CONFIG_PASSWORD = "Password";
+
     @Override
     public void onEnable() {
         getLogger().info("Starting up " + this.getName() + ". If you need me to update this plugin, email at gogobebe2@gmail.com");
+        saveDefaultConfig();
     }
 
     @Override
